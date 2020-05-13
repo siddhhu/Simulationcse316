68caa6f3ab30a578abb4b0915155c14eb08a8c73
Pradeep Suthar <49487927+sutharp777@users.noreply.github.com>
2020-05-13 22:30:17 +0530

    Update Firstcome.c

--- Firstcome.c
+++ Firstcome.c
@@ -3,38 +3,38 @@
 void FirstCome()                                                              //low Priority process -->First Come First Serve
 {
 	wait_time1[0] = 0;   
-    for(i=1;i<l;i++)
+    for (i=1; i<l; i++)
     {
         wait_time1[i] = 0;
-        for(p=0;p<l;p++)
+        for (p=0; p<l; p++)
         {
-            wait_time1[i]=wait_time1[i]+b_time3[p];
+            wait_time1[i] = wait_time1[i] + b_time3[p];
         }
     }
     
-    for(i=0;i<l;i++)
+    for (i=0; i<l; i++)
     {
-        turnaround_time3[i]=b_time3[i]+wait_time1[i];                               //As Turnaround time = Burst time + Waiting time
-        avg_wait_time3=avg_wait_time3+wait_time1[i];                            
-        avg_turn_time3=avg_turn_time3+turnaround_time3[i];
-        printf("\nProcess%d\t\t%d\t\t%d\t\t%d\n",i+1,b_time3[i],wait_time1[i],turnaround_time3[i]);
+        turnaround_time3[i] = b_time3[i] + wait_time1[i];                               //As Turnaround time = Burst time + Waiting time
+        avg_wait_time3 = avg_wait_time3 + wait_time1[i];                            
+        avg_turn_time3 = avg_turn_time3 + turnaround_time3[i];
+        printf("\nProcess%d\t\t%d\t\t%d\t\t%d\n", i+1, b_time3[i], wait_time1[i], turnaround_time3[i]);
     }
-    avg_wait_time3=avg_wait_time3/l;
-    avg_turn_time3=avg_turn_time3/l;
+    avg_wait_time3 = avg_wait_time3/l;
+    avg_turn_time3 = avg_turn_time3/l;
     
-    for(i=0;i<l;i++)
+    for (i=0; i<l; i++)
     {
-    	while(b_time3[i]!=0)
+    	while (b_time3[i] != 0)
     	{
-    		if(b_time3[i]>10)
+    		if (b_time3[i] > 10)
     		{
-				printf("\nProcess%d of Queue3 is running for 10 unit of time",i+1);
-				b_time3[i]=b_time3[i]-10;
+				printf("\nProcess%d of Queue3 is running for 10 unit of time", i+1);
+				b_time3[i] = b_time3[i]-10;
 			}
-			else if(b_time3[i]<=10)
+			else if (b_time3[i] <= 10)
 			{
-				printf("\nProcess%d of Queue2 is running for %d unit of time",i+1,b_time3[i]);
-				b_time3[i]=0;
+				printf("\nProcess%d of Queue2 is running for %d unit of time", i+1, b_time3[i]);
+				b_time3[i] = 0;
 			}
 		}
 	}
